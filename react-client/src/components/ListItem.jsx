e763b3afa42385d5c3294be251e10a038a1edcc5
Chetanpreet Malhi <chemalhi@gmail.com>
2017-08-19 16:36:04 -0400

    polishing

--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -40,49 +40,52 @@ export default class ListItem extends React.Component {
 
   render() {
     return (
-    	<div className="yelp-list-entry-container" style={this.state.style} onClick={(e)=> this.props.handleClick(this.props.item)}>
+    	<div className="yelp-list-entry-container" style={this.state.style} onClick={(e)=> {
+        e.preventDefault();
+        this.props.handleClick(this.props.item)
+      }}>
   	    <div className="yelp-list-entry">
           <div className="media-left media-middle">
             <img className="listing-object" src={this.props.item.image_url} alt="" />
-           </div>
+          </div>
           <div className="listing-body">
-            <a href={`http://maps.google.com/?daddr=${this.props.item.name} ${this.props.item.location.address1} ${this.props.item.location.city}&saddr=${this.props.startPoint.lat || ''},${this.props.startPoint.lng || ''}`}>
+            <a  target="_blank" href={`http://maps.google.com/?daddr=${this.props.item.name} ${this.props.item.location.address1} ${this.props.item.location.city}&saddr=${this.props.startPoint.lat || ''},${this.props.startPoint.lng || ''}`}>
   	          <div className="yelp-list-entry-name" >
                 {this.props.listkey + 1 + '. ' + this.props.item.name}
               </div>
             </a>
-            <div className="yelp-list-entry-rating">{this.props.item.rating}/5</div>
-            <div className="yelp-list-entry-price">{this.props.item.price}</div>
-          	<div className="yelp-list-entry-reviews">{this.props.item.review_count} Reviews</div>
+          <div className="yelp-list-entry-rating">{this.props.item.rating}/5</div>
+          <div className="yelp-list-entry-price">{this.props.item.price}</div>
+          <div className="yelp-list-entry-reviews">{this.props.item.review_count} Reviews</div>
+        </div>
+        <div className="yelp-list-entry-address">
+          <div>
+          	<div className="yelp-list-entry-address1">{this.props.item.location.address1}</div>
+          	<div className="yelp-list-entry-city">{this.props.item.location.city + ', ' + this.props.item.location.zip_code}</div>
+          	<div className="yelp-list-entry-phone">{this.props.item.phone}</div>
           </div>
-          <div className="yelp-list-entry-address">
-            <div>
-            	<div className="yelp-list-entry-address1">{this.props.item.location.address1}</div>
-            	<div className="yelp-list-entry-city">{this.props.item.location.city + ', ' + this.props.item.location.zip_code}</div>
-            	<div className="yelp-list-entry-phone">{this.props.item.phone}</div>
-            </div>
-            <div className="listItemBtns">
-              <button
-                className="listItemBtn"
-                onClick={this.stateClick}
-                value="yes"
-              >
-                Yes
-              </button>
-              <button
-                className="listItemBtn"
-                onClick={this.stateClick}
-                value="no"
-              >
-                No
-              </button>
-              <button
-                className="listItemBtn"
-                onClick={this.stateClick}
-                value="maybe"
-              >
-                Maybe
-              </button>
+          <div className="listItemBtns">
+            <button
+              className="listItemBtn"
+              onClick={this.stateClick}
+              value="yes"
+            >
+             Yes
+            </button>
+            <button
+              className="listItemBtn"
+              onClick={this.stateClick}
+              value="no"
+            >
+              No
+            </button>
+            <button
+              className="listItemBtn"
+              onClick={this.stateClick}
+              value="maybe"
+            >
+              Maybe
+            </button>
             </div>
           </div>
         </div>
